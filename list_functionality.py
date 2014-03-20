730eeb64de5867de9b07b3e34cf7ae50d673c1dd
Maria Krusteva <mimito_68@abv.bg>
2014-03-20 20:11:47 +0200

    adding merge_lists

--- list_functionality.py
+++ list_functionality.py
@@ -29,3 +29,18 @@ class CommandLineMailist():
         for i in range(0, len(list_of_files)):
             result += "[{}] - {}\n".format(i+1, list_of_files[i])
         return result
+
+    def merge_lists(list_identifier1, list_identifier2, new_list_name):
+        files = sorted(os.listdir("user_lists/")) #MUST BE GLOBAL
+
+        file1 = open("user_lists/" + files[list_identifier1-1], "r")
+        content1 = file1.read()
+        file1.close()
+
+        file2 = open("user_lists/" + files[list_identifier2-1], "r")
+        content2 = file2.read()
+        file2.close()
+
+        new_file = open("user_lists/" + new_list_name, "w")
+        new_file.write(content1 + "\n" + content2)
+        new_file.close()
