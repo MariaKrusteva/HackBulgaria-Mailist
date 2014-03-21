ff8c22c755fcc63b71ec680e4afeef94726e2786
Maria Krusteva <mimito_68@abv.bg>
2014-03-21 14:29:59 +0200

    Add update function

--- mailListAdapter.py
+++ mailListAdapter.py
@@ -1,9 +1,18 @@
 from maillist import MailList
 
-class MailListAdapter:
+
+class MailListAdapter(MailList):
     """docstring for MailListAdapter"""
     def __init__(self, name):
-        self.__mailList = MailLinst(name)
+        super().__init__(name)
 
-    def print_contacts:
-        pass
+    def show_list(self):
+        result = ""
+        i = 1
+        for key in sorted(self.contacts):
+            result += "[{}]".format(i) + key + " - " + self.contacts[key] + "\n"
+            i += 1
+        return result
+
+    def update(self, new_name):
+        self.name = new_name
